7a79d69f2d6735737744a0e1c3bfe47e17d68f20
Amrah Rustamov <amrahrustamov94@gmail.com>
2023-10-15 14:59:21 +0400

    new Todo added

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from './Layout/header.jsx';
+import Main from "./main.jsx";
+import Footer from "./Layout/footer.jsx";
+import './style.css';
+
+function App() {
+  return (
+    <>
+    <Header />
+    <Main />
+    <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const Main = () => {
+    const [todoArr,setTodoArr]=React.useState([])
+    const [todo,setTodo]=React.useState("")
+
+    const removeItem = (index) =>{
+        setTodoArr((oldArr)=>{
+            let newArr = [...oldArr];
+            newArr.splice(index,1);
+            return newArr;
+        })
+    }
+
+    function addTodo(e){
+        e.preventDefault();
+        console.log(todo.value)
+        const regex = /[a-zA-Z0-9]/;
+
+        if(regex.test(todo)){
+            setTodoArr([...todoArr,todo])
+            setTodo("");
+        }
+    }
+  return (
+    <div className="main">
+        <h3>Todo List</h3>
+        <form onSubmit={addTodo}>
+            <input type="text" placeholder='Add to Todo List something' value={todo} onChange={(e)=>setTodo(e.target.value)}/>
+            <button type='submit'>Add</button>
+        </form>
+        <ul>
+            {todoArr.map((todo,index)=> <li onClick={()=>removeItem(index)}><h5>{index + 1}</h5><p>{todo}</p></li>)}
+        </ul>
+    </div>
+  )
+}
+
+export default Main
